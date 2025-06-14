bb2ae61602fc9ce8214458330eb3a103cae0efac
KoKa <116951132+ahmedhamdy272@users.noreply.github.com>
2025-06-15 01:56:42 +0300

    Update Footer.tsx

--- components/main/Footer.tsx
+++ components/main/Footer.tsx
@@ -18,30 +18,30 @@ const Footer = () => {
             <div className="cursive font-bold text-[20px] pb-2">Community</div>
             <p className="flex flex-row items-center my-[15px] cursor-pointer">
               <FaYoutube />
-              <a href="https://www.youtube.com/@ahmedhamdy4336"></a>
+              <a href="https://www.youtube.com/@ahmedhamdy4336">Youtube</a>
             </p>
             <p className="flex flex-row items-center my-[15px] cursor-pointer">
               <RxGithubLogo />
-              <a href="https://github.com/ahmedhamdy272"></a>
+              <a href="https://github.com/ahmedhamdy272">Github</a>
             </p>
             <p className="flex flex-row items-center my-[15px] cursor-pointer">
               <RxDiscordLogo />
-              <a href="https://discord.com/"></a>
+              <a href="https://discord.com/">Discord</a>
             </p>
           </div>
           <div className="min-w-[200px] h-auto flex flex-col items-center justify-start">
             <div className="cursive font-bold text-[16px] pb-2">Social Media</div>
             <p className="flex flex-row items-center my-[15px] cursor-pointer">
               <RxInstagramLogo />
-              <a href="https://www.instagram.com/ah_med.ham_dy?igsh=MWE4cnZ4NzQwbnk2MQ=="></a>
+              <a href="https://www.instagram.com/ah_med.ham_dy?igsh=MWE4cnZ4NzQwbnk2MQ==">Instagram</a>
             </p>
             <p className="flex flex-row items-center my-[15px] cursor-pointer">
               <RxTwitterLogo />
-              <a href="https://x.com/AhmedHa87740668?t=aCCFApMn8hnHGt7cR1gc-g&s=09"></a>
+              <a href="https://x.com/AhmedHa87740668?t=aCCFApMn8hnHGt7cR1gc-g&s=09">Twitter</a>
             </p>
             <p className="flex flex-row items-center my-[15px] cursor-pointer">
               <RxLinkedinLogo />
-              <a href="www.linkedin.com/in/dev-ahmed-hamdi"></a>
+              <a href="www.linkedin.com/in/dev-ahmed-hamdi">Linkedin</a>
             </p>
           </div>
           <div className="min-w-[200px] h-auto flex flex-col items-center justify-start">
